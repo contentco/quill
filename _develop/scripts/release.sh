98b552d7a8c7c980d163c8ad114194fd569ad857
Ye Min <yehtut.it@gmail.com>
2017-10-10 18:59:35 +0800

    release 1.3.2.3

--- _develop/scripts/release.sh
+++ _develop/scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="$1"
+VERSION="1.3.2.3"
 
 if [ -z "$VERSION" ]; then
   echo "Version required."
@@ -20,8 +20,7 @@ cp dist/quill.core.css dist/quill.bubble.css dist/quill.snow.css dist/quill.js d
 
 cd .release
 
-printf "cdn: .\nversion: ." > jekyll.yml
-jekyll build -s ../docs -d _site --config ../docs/_config.yml,jekyll.yml
+
 
 mkdir quill/examples
 mv _site/standalone/bubble/index.html quill/examples/bubble.html
@@ -32,19 +31,3 @@ find quill/examples -type f -exec sed -i "" 's/href="\/\//href="https:\/\//g' {}
 find quill/examples -type f -exec sed -i "" 's/src="\/\//src="https:\/\//g' {} \;
 
 tar -czf quill.tar.gz quill
-
-aws s3 cp quill/quill.js s3://cdn.quilljs.com/$VERSION/ --cache-control max-age=604800 --content-type "application/javascript; charset=utf-8" --profile quill
-aws s3 cp quill/quill.min.js s3://cdn.quilljs.com/$VERSION/ --cache-control max-age=604800 --content-type "application/javascript; charset=utf-8" --profile quill
-aws s3 cp quill/quill.core.js s3://cdn.quilljs.com/$VERSION/ --cache-control max-age=604800 --content-type "application/javascript; charset=utf-8" --profile quill
-aws s3 cp quill/quill.bubble.css s3://cdn.quilljs.com/$VERSION/ --cache-control max-age=604800 --content-type "text/css; charset=utf-8" --profile quill
-aws s3 cp quill/quill.core.css s3://cdn.quilljs.com/$VERSION/ --cache-control max-age=604800 --content-type "text/css; charset=utf-8" --profile quill
-aws s3 cp quill/quill.snow.css s3://cdn.quilljs.com/$VERSION/ --cache-control max-age=604800 --content-type "text/css; charset=utf-8" --profile quill
-aws s3 cp quill/quill.min.js.map s3://cdn.quilljs.com/$VERSION/ --cache-control max-age=604800 --content-type "application/json; charset=utf-8" --profile quill
-aws s3 sync s3://cdn.quilljs.com/$VERSION/ s3://cdn.quilljs.com/latest/ --profile quill
-
-cd ..
-git tag v$VERSION -m "Version $VERSION"
-git push origin v$VERSION
-git push origin master
-
-npm publish
