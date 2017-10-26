2f067a77e0ddef5ea746c1150a3923081cbd3f86
Ye Min <yehtut.it@gmail.com>
2017-10-26 13:03:47 +0800

    create .release

--- _develop/scripts/release.sh
+++ _develop/scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="1.3.3.7"
+VERSION="1.3.3.8"
 
 if [ -z "$VERSION" ]; then
   echo "Version required."
@@ -11,14 +11,14 @@ fi
 
 rm -r .release
 rm -r dist
-mkdir release
-mkdir release/quill
+mkdir .release
+mkdir .release/quill
 
 npm run build
 webpack --config _develop/webpack.config.js --env.minimize
 cp dist/quill.core.css dist/quill.bubble.css dist/quill.snow.css dist/quill.js dist/quill.core.js dist/quill.min.js dist/quill.min.js.map .release/quill/
 
-cd release
+cd .release
 
 
 mkdir quill/examples
